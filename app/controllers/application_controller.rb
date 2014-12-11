859ac9537702f919b4fa801b41e9a5012cf3e244
Justin Schiff <AmbientRevolution@gmail.com>
2014-12-11 10:01:12 -0800

    make username actually work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  before_action :configure_permitted_paramaters, if: :devise_controller?
+  
   def index
   end
+  
+  private
+  
+  def configure_permitted_paramaters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
+  
 end
